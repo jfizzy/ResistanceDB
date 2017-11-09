84ec5e8c6f7a3e7e58e83a089998ee443e895b2a
James MacIsaac <james.macisaac@zone46-ec.cs.ucalgary.ca>
2017-11-09 15:02:31 -0700

    Added some started files for mia

---
+++ mia/mia.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+dest = os.environ.get('DEST_DIR')
+
+src_files = os.listdir(basedir)
+for file_name in src_files:
+    full_file_name = os.path.join(basedir, file_name)
+    if (os.path.isfile(full_file_name)):
+        shutil.copy(full_file_name, dest)
