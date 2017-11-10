3f38fd1310a53abcf42b6b4952eedb4cacb6855e
Tyrone Lagore <tyrone.lagore@ucalgary.ca>
2017-11-10 11:22:15 -0700

    I know this is the timed_intervals branch, but I added a logger to the program. filename is ez_logger, class name is EZLogger which utilizes the logging python library to create a log file and log messages to the file with a timestamp and the level of the message - ie, debug, warning, info, error, critical

---
+++ mia/ez_logger.py
@@ -0,0 +1,46 @@
+import time
+import datetime
+import logging
+import logging.handlers
+
+class EZLogger:
+    def __init__(self, logger_name, log_filename, filesize, backupCount, filemode):
+        self._log_name = logger_name
+        self._log_filename = log_filename
+        self._filesize = filesize
+        self._backupCount = backupCount
+
+        self._logger = logging.getLogger(self._log_name) 
+        self._logger.setLevel(logging.DEBUG)
+
+        handler = logging.handlers.RotatingFileHandler(\
+            self._log_filename, maxBytes=self._filesize, backupCount=self._backupCount)
+
+        self._logger.addHandler(handler)
+
+    def debug(self, msg, *args, **kwargs):
+        """ logs a debug messaege to the log file """
+        self._logger.debug(self.set_timestamp("DEBUG: {}".format(msg)), *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        """ logs an info message to the log file """
+        self._logger.info(self.set_timestamp("INFO: {}".format(msg)), *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        """ logs a warning message to the log file """
+        self._logger.warning(self.set_timestamp("WARNING: {}".format(msg)), *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        """ logs an error message to the log file """
+        self._logger.error(self.set_timestamp("ERROR: {}".format(msg)), *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        """ logs a critical message to the log file """
+        self._logger.critical(self.set_timestamp("CRITICAL: {}".format(msg)), *args, **kwargs)
+
+    def set_timestamp(self, msg):
+        """ prepends a timestamp to the message """
+        stamp = time.time()
+        formatted = st = datetime.datetime.fromtimestamp(stamp).strftime('%Y-%m-%d %H:%M:%S - ')
+        return "{}{}\n".format(formatted, msg)
+
--- mia/mia.py
+++ mia/mia.py
@@ -1,19 +1,59 @@
+from ez_logger import EZLogger
 import os
-import shutil
-from file_mover import FileMover
-from fileparser import FileParser
+from file_mover import FileMover, FileMoverException
+from fileparser import FileParser, FileParserException
 from config import Config
+
+logger = EZLogger(logger_name="MiaLogger", log_filename="mia_log.log", filesize=10*1024*1024, backupCount=5, filemode='w')
+
+def checkConfig(config):
+    """ checks that mia has received the default minimum valid arguments for config """
+    valid = True
+
+    if not config.SRC_DIRS:
+        logger.error("input configuration file has no Source Directories.\n"\
+            + "Please ensure there is at least one line like:\n"\
+            + "SRC_DIR=Full/Path/To/Source/Directory")
+        valid = False
+
+    if not config.DST_DIR:
+        logger.error("input configuration file has no destination directory.\n"\
+            + "Please ensure there is a line like:\n"\
+            + "DST_DIR=Full/Path/To/Destination/Directory")
+        valid = False
+
+    if not config.FILE_EXT:
+        logger.error("input configuration file has no file extension set."\
+            + "Please ensure there is a line like:\n"\
+            + "FILE_EXT=ext\nWhere 'ext' is the extension name of the files to move")
+        valid = False
+
+    if not config.CONVERTER:
+        logger.error("input configuration file has no Converter location set."\
+            + "Please ensure there is a line like:\n"\
+            + "CONVERTER=Full/Path/To/ReAdW.exe")
+        valid = False
+
+    return valid
+
 
 def main():
     """ main loop for mia program """
-    confg = Config(".miaconfig")
-    print(confg)
-    mover = FileMover(confg.SRC_DIRS, confg.INTERIM, confg.FILE_EXT, None)
-    mover.move_files()
-    
-    parser = FileParser(confg.INTERIM, confg.DST_DIR, confg.FILE_EXT, "mzXML", confg.CONVERTER, confg.CONVERTER_FLAGS)
-    parser.do_action()
+    config = Config(".miaconfig")
 
+    if not checkConfig(config):
+        exit(0)
+
+    try: 
+        mover = FileMover(config.SRC_DIRS, config.INTERIM, config.FILE_EXT, None)
+        mover.move_files()
+    
+        parser = FileParser(config.INTERIM, config.DST_DIR, config.FILE_EXT, "mzXML", config.CONVERTER, config.CONVERTER_FLAGS)
+        parser.do_action()
+    except FileMoverException as ex:
+        logger.error(ex)
+    except FileParserException as ex:
+        logger.error(ex)
 
 if __name__ == "__main__":
     main()
