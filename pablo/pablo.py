52251bcbebd583954bc2a5c33218abbd46cbd98b
James MacIsaac <james@jmproj.com>
2017-11-19 20:43:33 -0700

    have a basic script for the result visualization

--- pablo/pablo.py
+++ pablo/pablo.py
@@ -2,84 +2,64 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
-import pandas
-
-df = pandas.read_csv('https://gist.githubusercontent.com/chriddyp/' +
-                     '5d1ea79569ed194d432e56108a04d188/raw/' +
-                     'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-                     'gdp-life-exp-2007.csv')
+import pandas as pd
 
 app = dash.Dash()
 
-colors = {
-    'background': '#7FDBFF',
-    'text': '##00BFFF'
-}
+df = pd.read_csv('test.csv')
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(children='Hello Dash',
+sample_data = df.iloc[:,6:]
+
+app.layout = html.Div(children=[
+    html.H1(children='Pablo',
             style={
-                'textAlign': 'center',
-                'color': colors['text']
+            'textAlign': 'center'
             }
     ),
-
-    html.Div(children='Dash: A web application framework for Python.', 
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div([
-        dcc.Graph(
-            id='life-exp-vs-gdp',
-            figure={
-                'data': [
-                    go.Scatter(
-                        x=df[df['continent'] == i]['gdp per capita'],
-                        y=df[df['continent'] == i]['life expectancy'],
-                        text=df[df['continent'] == i]['country'],
-                        mode='markers',
-                        opacity=0.7,
-                        marker={
-                            'size': 15,
-                            'line': {'width': 0.5, 'color': 'white'}
-                        },
-                        name=i
-                    ) for i in df.continent.unique()
-                ],
-                'layout': go.Layout(
-                    xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                    yaxis={'title': 'Life Expectancy'},
-                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                    legend={'x': 0, 'y': 1},
-                    hovermode='closest'
-                    
-                )
-            }
-        )]
-    )
-])
-'''dcc.Graph(
-        id='example-graph-2',
+    dcc.Graph(
+        id='heatmap',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Heatmap(z=sample_data.values.tolist(),
+                    x=sample_data.columns.values.tolist(),
+                    y=df['compound'].tolist()
+                )
             ],
-            'layout': {
-                'title': 'Dash Data Visualization',
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
+            'layout': go.Layout(
+                xaxis={'title': 'Sample'},
+                yaxis={'title': 'rt_diff'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+            )
         }
-    )
-])'''
-
+    )]    
+)
+'''dcc.Graph(
+        id='medMz vs rt_diff',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=df[df['compound'] == i]['medMz'],
+                    y=df[df['compound'] == i]['rt_diff'],
+                    text=df[df['compound'] == i]['compound'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df['compound']
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'medMz'},
+                yaxis={'title': 'rt_diff'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    ),'''
 
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
