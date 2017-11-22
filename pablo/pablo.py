9f4dbbef3e4ed2bc990588569f8d8de4e8ad2469
James MacIsaac <james@jmproj.com>
2017-11-21 19:00:50 -0700

    Added a basic scatter plot and table which are kind of ugly but give the gist of it

--- pablo/pablo.py
+++ pablo/pablo.py
@@ -1,67 +1,78 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
+
 import pandas as pd
-
-app = dash.Dash()
-
-df = pd.read_csv('test.csv')
-
-sample_data = df.iloc[:,6:]
-
-app.layout = html.Div(children=[
-    html.H1(children='Pablo',
-            style={
-            'textAlign': 'center'
-            }
-    ),
-    dcc.Graph(
-        id='heatmap',
-        figure={
-            'data': [
-                go.Heatmap(z=sample_data.values.tolist(),
-                    x=sample_data.columns.values.tolist(),
-                    y=df['compound'].tolist()
+        
+class DataWrapper:
+    def __init__(self):
+        self._df = pd.read_csv('test.csv')
+        self._x_axis = self._df['compound']
+        self._samples = self._df.iloc[:,6:]
+        
+def main():
+    dw = DataWrapper()
+    app = dash.Dash()
+    app.layout = html.Div(children=[
+        html.H1(children='Hello Dash'),
+    
+        html.Div(children='''
+            Dash: A web application framework for Python.'''
+        ),
+    
+        dcc.Graph(
+            id='line-plot',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=dw._x_axis.tolist(),
+                        y=dw._samples.values.tolist()[i],
+                        text=dw._df[dw._df['rt_diff'] == i]['rt_diff'],
+                        mode='markers',
+                        opacity=0.7,
+                        marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                        },
+                        name=dw._samples.values.tolist()[i][0]
+                    ) for i in dw._samples.index
+                ],
+                'layout': go.Layout(
+                    xaxis={'title': 'Compound', 'autorange': True},
+                    yaxis={'title': 'Intensity', 'autorange': True},
+                    margin={'l': 5, 'b': 10, 't': 10, 'r': 5},
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest',
+                    width=1600  
                 )
-            ],
-            'layout': go.Layout(
-                xaxis={'title': 'Sample'},
-                yaxis={'title': 'rt_diff'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-            )
-        }
-    )]    
-)
-'''dcc.Graph(
-        id='medMz vs rt_diff',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['compound'] == i]['medMz'],
-                    y=df[df['compound'] == i]['rt_diff'],
-                    text=df[df['compound'] == i]['compound'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df['compound']
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'medMz'},
-                yaxis={'title': 'rt_diff'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    ),'''
-
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-
-if __name__ == '__main__':
+            }
+        ),
+        
+        html.Div(children=[
+            html.H4(children='Table with the Data'),
+            generate_table(dw._samples)
+        ])
+    ])
+        
     app.run_server(debug=True)
+    app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+    
+def generate_table(df):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in df.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(df.iloc[i][col]) for col in df.columns
+        ]) for i in range(len(df))]
+    )
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
