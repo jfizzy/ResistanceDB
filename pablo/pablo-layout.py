26dde6e086a5780c96de064b6ddd2b6eb166e67a
James MacIsaac <james@jmproj.com>
2017-11-23 18:40:03 -0700

    Adding in the better layout pablo

--- pablo/pablo-layout.py
+++ pablo/pablo-layout.py
@@ -1,6 +1,10 @@
 # standard library
 import os
 
+# flask
+import flask
+from flask import send_from_directory
+
 # dash libs
 import dash
 from dash.dependencies import Input, Output
@@ -14,39 +18,149 @@ import pandas as pd
 #from sqlalchemy import create_engine
 
 # set params
-#conn = create_engine(os.environ['DB_URI'])
+data_file = 'test.csv'
 
+# dash stylesheet
+stylesheet = 'https://codepen.io/chriddyp/pen/bWLwgP.css'
 
 ###########################
 # Data Manipulation / Model
 ###########################
 
-def fetch_data(q):
-    df = pd.read_sql(
-        sql=q,
-        con=conn
-    )
-    return df
+class DataWrapper:
+    def __init__(self):
+        self.df = self.fetch_data(data_file)
+        self.compounds = self.get_compounds(self.df)
+        self.sample_matrix = self.get_sample_matrix(self.df)
 
+    def get_compounds_list(self):
+        '''returns the compounds attribute contents as a list'''
+        return self.compounds.tolist()
+
+    def get_sample_names(self):
+        '''returns the sample names as a list'''
+        return self.df.columns.get_values().tolist()[6:]
+
+    def get_sample_lists(self):
+        '''returns the samples as a list of lists'''
+        return self.sample_matrix.values.tolist()
+
+    def fetch_data(self, f):
+        df = pd.read_csv(f)
+        return df
+
+    def get_compounds(self, df):
+        '''returns the compounds that are listed in the data file'''
+        return df['compound']
+
+    def get_sample_matrix(self, df):
+        return df.ix[:,6:]
+
+def draw_sample_graph(sample):
+    pass
 
 #########################
 # Dashboard Layout / View
 #########################
 
+def generate_table(df, max_rows=10):
+    '''Given dataframe, return template generated using Dash components
+    '''
+
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in df.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(df.iloc[i][col]) for col in df.columns
+        ]) for  i in range(min(len(df), max_rows))]
+    )
+
+def onLoad_sample_options(dw):
+    '''Actions to perform upon initial page load'''
+    sample_options = (
+        [{'label': sample, 'value': index}
+         for index,sample in enumerate(dw.get_sample_names())]
+    )
+    return sample_options
+
 # Set up Dashboard and create layout
 app = dash.Dash()
-app.css.append_css({
-    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
 
+# allow stylesheet serving
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+# parse the data file
+dw = DataWrapper()
+
+# set up the page layout
 app.layout = html.Div([
+
+    # Default Styles
+    html.Link(
+        rel='stylesheet',
+        href=stylesheet
+    ),
+
+    # Custom Styles
+    html.Link(
+        rel='stylesheet',
+        href='/static/style.css'
+    ),
 
     # Page Header
     html.Div([
-        html.H1('Project Header')
+        html.H1(children='Mass Spec Sample Viewer',
+                style={
+                    'textAlign': 'center'
+                })
     ]),
 
+    # Comparison Grid
+    html.Div([
+        html.Div([
+            dcc.Graph(id='comparison-graph'),
+        ])
+    ], className='twelve columns'),
+
+    # Dropdown Grid
+    html.Div([
+        html.Div([
+            # Select Sample Dropdown
+            html.Div([
+                html.Div('Sample', className='three columns'),
+                html.Div(dcc.Dropdown(
+                    id='sample-selector',
+                    options=onLoad_sample_options(dw)),
+                    className='nine columns')
+            ]),
+        ], className='six columns'),
+
+        # Sample Summary
+        html.Div([
+
+            # Sample Graph
+            dcc.Graph(id='sample-graph')
+        ], className='six columns'),
+
+    # Table Grid
+    html.Div([
+        # Sample Table
+        html.Table(
+
+        )
+    ], className='tewlve columns')
+
+    ], className='twelve columns'),
 ])
+
+# serve custom stylesheets
+@app.server.route('/static/<path:path>')
+def static_file(path):
+    static_folder = os.path.join(os.getcwd(), 'static')
+    return send_from_directory(static_folder, path)
 
 
 #############################################
