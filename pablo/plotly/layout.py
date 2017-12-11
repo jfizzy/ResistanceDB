88fc1a532fc247be3c9123e2c7053a624ade7883
James MacIsaac <james@jmproj.com>
2017-12-11 13:37:33 -0700

    Updated pablo to generate the 4 wanted graphs

--- pablo/plotly/layout.py
+++ pablo/plotly/layout.py
@@ -1,6 +1,7 @@
 import sys
 import plotly.offline as py
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
 import pandas as pd
 
 import numpy as np
@@ -50,7 +51,7 @@ class GraphUtil:
         self.data = [go.Heatmap(z=sample_matrix.values.tolist(),
                                 x=sample_names,
                             y=compounds.values.tolist(),
-                            colorscale='RdBu',
+                            colorscale='Viridis',
                             )
                     ]
 
@@ -65,40 +66,70 @@ class GraphUtil:
             height=self.num_compounds * self.row_height,
         )
 
-    def generate_3dribbon_trace(self, compounds, sample_names, sample_matrix):
+    def generate_annotated_heatmap_trace(self, compounds, sample_names, sample_matrix):
         self.num_compounds = len(compounds.values.tolist())
         self.num_samples = len(sample_names)
+
         z = sample_matrix.values.tolist()
+        x = sample_names
+        y = compounds.values.tolist()
+        z2 = []
+        for s in z:
+            items=[]
+            for item in s:
+                if item > 0:
+                    items.append(1)
+                elif item < 0:
+                    items.append(-1)
+                else:
+                    items.append(0)
+            z2.append(items)
+        self.data = [go.Heatmap(z=z2,
+                                x=x,
+                                y=y,
+                                colorscale='RdBu',
+                                text=z,
+                                hoverinfo='x+y+text',
+                                reversescale=True
+                            )
+                    ]
 
-        traces=[]
+        self.layout = go.Layout(
+            title='Sample Intensities',
+            autosize=True,
+            xaxis=dict(type='category', categoryarray=sample_names, categoryorder='array', showgrid=True),
+            yaxis=dict(type='category', categoryorder='index', categoryarray=compounds.values.tolist(), dtick=1),
+            shapes=self.draw_heatmap_shapes(),
 
-        y_raw=compounds.values.tolist()
-        print(y_raw)
+            height=self.num_compounds * self.row_height,
+        )
 
-        for i in range(0, self.num_samples):
-            z_raw = z[i]
-            print(z_raw)
-            x = []
-            y = []
-            z = []
-            ci = int(255/self.num_samples*i) # colors
+    def generate_3dsurface_trace(self, compounds, sample_names, sample_matrix):
+        self.num_compounds = len(compounds.values.tolist())
+        self.num_samples = len(sample_names)
 
-            for j in range(0, self.num_compounds):
-                z.append([z_raw[j], z_raw[j]])
-                y.append([y_raw[j], y_raw[j]])
-                x.append([i*2, i*2+1])
-            traces.append(dict(
-                z=z,
-                x=x,
-                y=y,
-                colorscale=[ [i, 'rgb(%d,%d,255)'%(ci, ci)] for i in np.arange(0,1.1,0.1) ],
-                showscale=True,
-                type='surface'
-            ))
+        min_mz = self.min_value(sample_matrix)
+        max_mz = self.max_value(sample_matrix)
 
+        self.data = [
+            go.Surface(
+                z=sample_matrix.values.tolist(),
+                x=sample_names,
+                y=compounds.values.tolist(),
+                colorscale='Viridis',
+            )
+        ]
 
-        fig = go.Figure(data=traces, layout={'title': 'Ribbon Plot of Intensities'})
-        py.plot(fig, filename='3dRibbon.html')
+        self.layout = go.Layout(
+            scene=dict(
+                xaxis=dict(type='category', categoryarray=sample_names, categoryorder='array', dtick=1, title='Sample'),
+                yaxis=dict(type='category', categoryorder='index', categoryarray=compounds.values.tolist(), dtick=1, title='Compound ({})'.format(self.num_compounds)),
+                zaxis=dict(title='MZ')
+            ),
+            margin=dict(
+            r=20, l=10,
+            b=10, t=10)
+        )
 
 
     @staticmethod
@@ -109,14 +140,16 @@ class GraphUtil:
     def min_value(inputlist):
         return min([sublist[-1] for sublist in inputlist])
 
-    def generate_graph(self):
+    def generate_graph(self, filename):
         fig = go.Figure(data=self.data, layout=self.layout)
-        py.plot(fig, filename='heatmap.html')
+        py.plot(fig, filename=filename)
 
     def draw_heatmap_shapes(self):
         shapes_v = [self.make_vertical_line(x) for x in range(0, self.num_samples+1)[::2]]
+        seps_v = [self.make_seperator_line(x) for x in range(1, self.num_samples)[::2]]
         shapes_h = [self.make_horizontal_line(y) for y in range(0, self.num_compounds+1)]
         shapes_v.extend(shapes_h)
+        shapes_v.extend(seps_v)
         return shapes_v
 
     def make_vertical_line(self, x):
@@ -128,6 +161,18 @@ class GraphUtil:
               'line': {
                 'color': 'black',
                 'width': 2,
+            }})
+
+    def make_seperator_line(self, x):
+        return dict({'type': 'line',
+              'x0': x-0.5,
+              'y0': -0.5,
+              'x1': x-0.5,
+              'y1': self.num_compounds-0.5,
+              'line': {
+                'color': 'dark grey',
+                'width': 2,
+                'dash': 'dashdot'
             }})
 
     def make_horizontal_line(self, y):
@@ -143,18 +188,64 @@ class GraphUtil:
             },
         })
 
+    def generate_scatter_plot(self, compounds, sample_names, sample_matrix):
+
+        self.data = []
+        index = 0
+        for sample in sample_matrix.T.values.tolist():
+            name = sample_names[index]
+            for value in sample:
+                self.data.append(
+                    go.Scatter(x=compounds.values.tolist(),
+                               y=value,
+                               mode='markers+lines',
+                               opacity=0.7,
+                               marker=dict(
+                                   size=15,
+                                   line=dict(width=0.5, color='white')
+                               ),
+                               name=name,
+                               )
+                )
+            index = index + 1
+
+        self.data = [
+            go.Scatter(x=compounds.values.tolist(),
+                       y=sample,
+                       mode='markers',
+                       opacity=0.7,
+                       marker=dict(
+                           size=15,
+                           line=dict(width=0.5, color='white')
+                       ),
+                       name=name,
+                       ) for sample, name in zip(sample_matrix.T.values.tolist(), sample_names)
+            ]
+
+        self.layout=go.Layout(
+            title='Sample Compound Intensities',
+            showlegend=True,
+            xaxis=dict(type='category', categoryarray=compounds.values.tolist(), categoryorder='array', dtick=1, title='Sample'),
+            yaxis=dict(type='range', title='MZ'),
+        )
+
 def tbone_main(output_file, condensed_file=None):
     o_dw = DataWrapper(output_file)
 
     gu = GraphUtil()
     gu.generate_heatmap_trace(o_dw.compounds, o_dw.get_sample_names(), o_dw.get_sample_matrix())
-    gu.generate_graph()
+    gu.generate_graph('heatmap.html')
+    gu.generate_scatter_plot(o_dw.compounds, o_dw.get_sample_names(), o_dw.get_sample_matrix())
+    gu.generate_graph('scatterplot.html')
 
-    gu.generate_3dribbon_trace(o_dw.compounds, o_dw.get_sample_names(), o_dw.get_sample_matrix())
     if condensed_file is not None:
         c_dw = DataWrapper(condensed_file)
         gu = GraphUtil()
-        gu.generate_3dribbon_trace(c_dw.compounds, c_dw.get_sample_names(), c_dw.get_sample_matrix())
+        gu.generate_3dsurface_trace(c_dw.compounds, c_dw.get_sample_names(), c_dw.get_sample_matrix())
+        gu.generate_graph('3dsurface.html')
+
+        gu.generate_annotated_heatmap_trace(c_dw.compounds, c_dw.get_sample_names(), c_dw.get_sample_matrix())
+        gu.generate_graph('condensed_heatmap.html')
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
